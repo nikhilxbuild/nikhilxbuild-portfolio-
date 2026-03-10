19fd0856365d81a7d965305df0cd24968d089429
Nikhil Sinha <nikhil.sinha380@gmail.com>
2026-03-10 19:30:56 +0530

    Revise content and skills in WhatIDo component
    Updated descriptions and skillset in WhatIDo component.

--- src/components/WhatIDo.tsx
+++ src/components/WhatIDo.tsx
@@ -90,21 +90,16 @@ const WhatIDo = () => {
               <h3>DEVELOP</h3>
               <h4>Description</h4>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-                quia aliquid laboriosam ducimus sit molestiae.
+                I build modern web applications, AI-powered tools, and developer utilities focused on automation, productivity, and scalable software systems.
               </p>
               <h5>Skillset & tools</h5>
               <div className="what-content-flex">
                 <div className="what-tags">JavaScript</div>
                 <div className="what-tags">TypeScript</div>
-                <div className="what-tags">Three.js</div>
                 <div className="what-tags">React</div>
-                <div className="what-tags">Css</div>
-                <div className="what-tags">Node.js</div>
                 <div className="what-tags">Next.js</div>
-                <div className="what-tags">Express.js</div>
-                <div className="what-tags">PHP</div>
-                <div className="what-tags">MySql</div>
+                <div className="what-tags">Node.js</div>
+                <div className="what-tags">MySQL</div>
               </div>
               <div className="what-arrow"></div>
             </div>
@@ -128,22 +123,17 @@ const WhatIDo = () => {
             </div>
             <div className="what-corner"></div>
             <div className="what-content-in">
-              <h3>DESIGN</h3>
+              <h3>PRODUCT</h3>
               <h4>Description</h4>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-                quia aliquid laboriosam ducimus sit molestiae
+                Focused on product thinking, system architecture, and building intuitive user interfaces for web products and developer tools.
               </p>
               <h5>Skillset & tools</h5>
               <div className="what-content-flex">
-                <div className="what-tags">Blender</div>
-                <div className="what-tags">Zbrush</div>
                 <div className="what-tags">UI Design</div>
-                <div className="what-tags">Motion</div>
-                <div className="what-tags">Rigging</div>
-                <div className="what-tags">3D Animation</div>
-                <div className="what-tags">Character Design</div>
-                <div className="what-tags">Modelling</div>
+                <div className="what-tags">System Design</div>
+                <div className="what-tags">Product Development</div>
+                <div className="what-tags">Software Architecture</div>
               </div>
               <div className="what-arrow"></div>
             </div>
@@ -170,3 +160,4 @@ function handleClick(container: HTMLDivElement) {
     });
   }
 }
+
